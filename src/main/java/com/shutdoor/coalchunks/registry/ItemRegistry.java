3725fd8ed09265b35f06aadf0d3c468b0f261535
shutdoor <enyssa2129@gmail.com>
2022-04-03 13:31:57 -0500

    Port 1.17.1 to 1.18.1

--- src/main/java/com/shutdoor/coalchunks/registry/ItemRegistry.java
+++ src/main/java/com/shutdoor/coalchunks/registry/ItemRegistry.java
@@ -4,9 +4,9 @@ import com.shutdoor.coalchunks.CoalChunks;
 import com.shutdoor.coalchunks.items.FuelItem;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @SuppressWarnings("unused")
 public class ItemRegistry {
