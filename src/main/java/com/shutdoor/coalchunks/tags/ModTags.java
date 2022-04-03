dd93347dcb914444fd6d0001d498e05beed9c838
shutdoor <enyssa2129@gmail.com>
2022-04-03 15:15:40 -0500

    Port 1.18.1 to 1.18.2

--- src/main/java/com/shutdoor/coalchunks/tags/ModTags.java
+++ src/main/java/com/shutdoor/coalchunks/tags/ModTags.java
@@ -5,25 +5,23 @@ import com.shutdoor.coalchunks.registry.ItemRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.Tag;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.Tags;
 
 public class ModTags {
     public static final class Items {
-        public static final Tags.IOptionalNamedTag<Item> LARGE_COAL_CHUNK = createModTag(ItemRegistry.LARGE_COAL_CHUNK.get());
-        public static final Tags.IOptionalNamedTag<Item> MEDIUM_COAL_CHUNK = createModTag(ItemRegistry.MEDIUM_COAL_CHUNK.get());
-        public static final Tags.IOptionalNamedTag<Item> SMALL_COAL_CHUNK = createModTag(ItemRegistry.SMALL_COAL_CHUNK.get());
+        public static final TagKey<Item> LARGE_COAL_CHUNK = createModTag(ItemRegistry.LARGE_COAL_CHUNK.get());
+        public static final TagKey<Item> MEDIUM_COAL_CHUNK = createModTag(ItemRegistry.MEDIUM_COAL_CHUNK.get());
+        public static final TagKey<Item> SMALL_COAL_CHUNK = createModTag(ItemRegistry.SMALL_COAL_CHUNK.get());
 
-        public static final Tags.IOptionalNamedTag<Item> LARGE_CHARCOAL_CHUNK = createModTag(ItemRegistry.LARGE_CHARCOAL_CHUNK.get());
-        public static final Tags.IOptionalNamedTag<Item> MEDIUM_CHARCOAL_CHUNK = createModTag(ItemRegistry.MEDIUM_CHARCOAL_CHUNK.get());
-        public static final Tags.IOptionalNamedTag<Item> SMALL_CHARCOAL_CHUNK = createModTag(ItemRegistry.SMALL_CHARCOAL_CHUNK.get());
+        public static final TagKey<Item> LARGE_CHARCOAL_CHUNK = createModTag(ItemRegistry.LARGE_CHARCOAL_CHUNK.get());
+        public static final TagKey<Item> MEDIUM_CHARCOAL_CHUNK = createModTag(ItemRegistry.MEDIUM_CHARCOAL_CHUNK.get());
+        public static final TagKey<Item> SMALL_CHARCOAL_CHUNK = createModTag(ItemRegistry.SMALL_CHARCOAL_CHUNK.get());
 
-
-        private static Tag.Named<Item> forge(String path) { return ItemTags.bind("forge:" + path); }
-
-        private static Tags.IOptionalNamedTag<Item> createModTag(Item item) {
+        private static TagKey<Item> createModTag(Item item) {
             String name = item.asItem().getRegistryName().toString();
-            return ItemTags.createOptional(new ResourceLocation(CoalChunks.MODID, name));
+            return ItemTags.create(new ResourceLocation(CoalChunks.MODID, name));
         }
     }
 
