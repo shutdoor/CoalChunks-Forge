995c5bfd0057a893ef4277d5942f439b196ce1ef
shutdoor <enyssa2129@gmail.com>
2022-04-03 11:32:08 -0500

    Updated from 1.16.5 to 1.17.1

--- src/main/java/com/shutdoor/coalchunks/items/FuelItem.java
+++ src/main/java/com/shutdoor/coalchunks/items/FuelItem.java
@@ -1,12 +1,14 @@
 package com.shutdoor.coalchunks.items;
 
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.property.Properties;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -14,23 +16,23 @@ import java.util.List;
 public class FuelItem extends Item {
 
     public final int burnTime;
-    public final TranslationTextComponent hoverText;
+    public final TranslatableComponent hoverText;
 
-    public FuelItem(Properties properties, int burn, TranslationTextComponent text) {
-        super(properties);
+    public FuelItem(Properties properties, int burn, TranslatableComponent text) {
+        super(properties.tab(CreativeModeTab.TAB_MISC));
         this.burnTime = burn;
         this.hoverText = text;
     }
 
     @Override
-    public int getBurnTime(ItemStack itemStack) {
+    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
         return burnTime;
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        tooltip.add(hoverText);
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComp, TooltipFlag isAdvance) {
+        tooltipComp.add(hoverText);
 
-        super.appendHoverText(stack, worldIn, tooltip, flagIn);
+        super.appendHoverText(stack, level, tooltipComp, isAdvance);
     }
 }
--- src/main/java/com/shutdoor/coalchunks/registry/ItemRegistry.java
+++ src/main/java/com/shutdoor/coalchunks/registry/ItemRegistry.java
@@ -2,10 +2,9 @@ package com.shutdoor.coalchunks.registry;
 
 import com.shutdoor.coalchunks.CoalChunks;
 import com.shutdoor.coalchunks.items.FuelItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -14,26 +13,26 @@ public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CoalChunks.MODID);
 
     public static final RegistryObject<FuelItem> SMALL_COAL_CHUNK = ITEMS.register(
-            "small_coal_chunk", () -> new FuelItem(new Item.Properties().tab(ItemGroup.TAB_MISC), 200,
-                    new TranslationTextComponent("tooltip.coalchunks.small_coal_chunk")));
+            "small_coal_chunk", () -> new FuelItem(new Item.Properties(), 200,
+                    new TranslatableComponent("tooltip.coalchunks.small_chunk")));
 
     public static final RegistryObject<FuelItem> SMALL_CHARCOAL_CHUNK = ITEMS.register(
-            "small_charcoal_chunk", () -> new FuelItem(new Item.Properties().tab(ItemGroup.TAB_MISC), 200,
-                    new TranslationTextComponent("tooltip.coalchunks.small_charcoal_chunk")));
+            "small_charcoal_chunk", () -> new FuelItem(new Item.Properties(), 200,
+                    new TranslatableComponent("tooltip.coalchunks.small_chunk")));
 
     public static final RegistryObject<FuelItem> MEDIUM_COAL_CHUNK = ITEMS.register(
-            "medium_coal_chunk", () -> new FuelItem(new Item.Properties().tab(ItemGroup.TAB_MISC), 400,
-                    new TranslationTextComponent("tooltip.coalchunks.medium_coal_chunk")));
+            "medium_coal_chunk", () -> new FuelItem(new Item.Properties(), 400,
+                    new TranslatableComponent("tooltip.coalchunks.medium_chunk")));
 
     public static final RegistryObject<FuelItem> MEDIUM_CHARCOAL_CHUNK = ITEMS.register(
-            "medium_charcoal_chunk", () -> new FuelItem(new Item.Properties().tab(ItemGroup.TAB_MISC), 400,
-                    new TranslationTextComponent("tooltip.coalchunks.medium_charcoal_chunk")));
+            "medium_charcoal_chunk", () -> new FuelItem(new Item.Properties(), 400,
+                    new TranslatableComponent("tooltip.coalchunks.medium_chunk")));
 
     public static final RegistryObject<FuelItem> LARGE_COAL_CHUNK = ITEMS.register(
-            "large_coal_chunk", () -> new FuelItem(new Item.Properties().tab(ItemGroup.TAB_MISC), 800,
-                    new TranslationTextComponent("tooltip.coalchunks.large_coal_chunk")));
+            "large_coal_chunk", () -> new FuelItem(new Item.Properties(), 800,
+                    new TranslatableComponent("tooltip.coalchunks.large_chunk")));
 
     public static final RegistryObject<FuelItem> LARGE_CHARCOAL_CHUNK = ITEMS.register(
-            "large_charcoal_chunk", () -> new FuelItem(new Item.Properties().tab(ItemGroup.TAB_MISC), 800,
-                    new TranslationTextComponent("tooltip.coalchunks.large_charcoal_chunk")));
+            "large_charcoal_chunk", () -> new FuelItem(new Item.Properties(), 800,
+                    new TranslatableComponent("tooltip.coalchunks.large_chunk")));
 }
